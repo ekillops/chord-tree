ed7dc6598048a8ac8db944a29fcc6c4387c82196
Erik Killops and Ezra Sandzer-Bell <ezra@>
2017-01-24 10:47:15 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    TreeComponent
   ],
   imports: [
     BrowserModule,
