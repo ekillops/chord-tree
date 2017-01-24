43f37ee9315e2851294b1fc6de5982ff010e90fc
Erik Killops and Ezra Sandzer-Bell <ezra@>
2017-01-24 11:26:02 -0800

    add app.routing.ts with index and tree routes

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { TreeComponent } from './tree/tree.component';
+
+const appRoutes: Routes = [
+{
+  path: '',
+  component: WelcomeComponent
+},
+
+{
+  path: 'tree',
+  component: TreeComponent
+}
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
